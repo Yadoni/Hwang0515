c1d9b856aabfacd948e4033ef0021b437a0462c9
Yadoni <ygkim2024@gmail.com>
2025-05-13 10:38:13 +0900

    Update app.py

--- view_app/app.py
+++ view_app/app.py
@@ -28,7 +28,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.title("💐 스승의 날 메시지 시각화 대시보드")
+st.title("💐 2025 스승의 날 맞이 황승식 교수님께")
 
 # === 데이터 로딩 ===
 records = sheet.get_all_records()
